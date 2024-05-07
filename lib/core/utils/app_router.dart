dd0e691f93ef714c005d8b0484c5e2c4887a5ace
mohamad darweshe <@gmail.com>
2024-05-07 12:21:59 +0300

    add images

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,14 +1,14 @@
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 
-abstract class AppRouter {
-  static const kHomeView = '/HomeView';
+// abstract class AppRouter {
+//   static const kHomeView = '/HomeView';
 
-  static final router = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => const (),
-      ),
-    ],
-  );
-}
+//   static final router = GoRouter(
+//     routes: [
+//       GoRoute(
+//         path: '/',
+//         builder: (context, state) => const (),
+//       ),
+//     ],
+//   );
+// }
