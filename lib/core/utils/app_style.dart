aea0e40f77fe7296464a4658ea9e4bc53d801a1c
mohamad darweshe <@gmail.com>
2024-05-07 13:14:37 +0300

    updete app_style

--- lib/core/utils/app_style.dart
+++ lib/core/utils/app_style.dart
@@ -1,51 +1,51 @@
 import 'package:flutter/widgets.dart';
 
 abstract class AppStyles {
-  static const TextStyle StyleOpenSansBold20 = TextStyle(
+  static const TextStyle styleOpenSansBold20 = TextStyle(
     fontSize: 20,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle StyleOpenSansBold16 = TextStyle(
+  static const TextStyle styleOpenSansBold16 = TextStyle(
     fontSize: 16,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle StyleOpenSansBold12 = TextStyle(
+  static const TextStyle styleOpenSansBold12 = TextStyle(
     fontSize: 12,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle StyleOpenSansBold10 = TextStyle(
+  static const TextStyle styleOpenSansBold10 = TextStyle(
     fontSize: 10,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle StyleOpenSansRegular10 = TextStyle(
+  static const TextStyle styleOpenSansRegular10 = TextStyle(
     fontSize: 10,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.w500,
   );
-  static const TextStyle StyleUrbanistMedium14 = TextStyle(
+  static const TextStyle styleUrbanistMedium14 = TextStyle(
     fontSize: 14,
     fontFamily: "Urbanist",
     fontWeight: FontWeight.normal,
   );
 
-  static const TextStyle StyleOpenSansBold14 = TextStyle(
+  static const TextStyle styleOpenSansBold14 = TextStyle(
     fontSize: 14,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.bold,
   );
-  static const TextStyle StyleOpenSansRegular24 = TextStyle(
+  static const TextStyle styleOpenSansRegular24 = TextStyle(
     fontSize: 24,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.w500,
   );
-  static const TextStyle StyleOpenSansRegular8 = TextStyle(
+  static const TextStyle styleOpenSansRegular8 = TextStyle(
     fontSize: 8,
     fontFamily: "OpenSans",
     fontWeight: FontWeight.w500,
