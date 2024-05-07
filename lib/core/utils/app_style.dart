4fe2bf5a02f65fa2db6fbbd687e61b7da4d0e311
mohamad darweshe <@gmail.com>
2024-05-07 11:43:31 +0300

    add app style

--- lib/core/utils/app_style.dart
+++ lib/core/utils/app_style.dart
@@ -1,1 +1,53 @@
-abstract class AppStyles {}
+import 'package:flutter/widgets.dart';
+
+abstract class AppStyles {
+  static const TextStyle StyleOpenSansBold20 = TextStyle(
+    fontSize: 20,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle StyleOpenSansBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle StyleOpenSansBold12 = TextStyle(
+    fontSize: 12,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle StyleOpenSansBold10 = TextStyle(
+    fontSize: 10,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle StyleOpenSansRegular10 = TextStyle(
+    fontSize: 10,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle StyleUrbanistMedium14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "Urbanist",
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle StyleOpenSansBold14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle StyleOpenSansRegular24 = TextStyle(
+    fontSize: 24,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle StyleOpenSansRegular8 = TextStyle(
+    fontSize: 8,
+    fontFamily: "OpenSans",
+    fontWeight: FontWeight.w500,
+  );
+}
