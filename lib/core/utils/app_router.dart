8feb855b9337c9ae67bb30a9267b7c5ef5b0acba
mohamad darweshe <@gmail.com>
2024-05-07 13:33:17 +0300

    update app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,14 +1,33 @@
-// import 'package:go_router/go_router.dart';
+import 'package:doptica_app/featurs/edit_profile_feature.dart/edit_profile_view.dart';
+import 'package:doptica_app/featurs/home_feature/home_view.dart';
+import 'package:doptica_app/featurs/profile_feature/profile_view.dart';
+import 'package:doptica_app/featurs/tasks_featurs/tasks_view.dart';
+import 'package:go_router/go_router.dart';
 
-// abstract class AppRouter {
-//   static const kHomeView = '/HomeView';
+abstract class AppRouter {
+  static const kHomeView = '/';
+  static const kEditProfileView = '/EditProfileView';
+  static const kProfileView = '/ProfileView';
+  static const kTasksView = '/TasksView';
 
-//   static final router = GoRouter(
-//     routes: [
-//       GoRoute(
-//         path: '/',
-//         builder: (context, state) => const (),
-//       ),
-//     ],
-//   );
-// }
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kEditProfileView,
+        builder: (context, state) => const EditProfileView(),
+      ),
+      GoRoute(
+        path: kProfileView,
+        builder: (context, state) => const ProfileView(),
+      ),
+      GoRoute(
+        path: kTasksView,
+        builder: (context, state) => const TasksView(),
+      ),
+    ],
+  );
+}
