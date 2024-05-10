e4e0152bd7afdad89715f246affcfe099636b57e
abdalazez <abdalazezk@gmail.com>
2024-05-10 16:30:47 +0300

    added sign in with google button + new font style

--- lib/featurs/signup_and_signin.dart/sign_up.dart
+++ lib/featurs/signup_and_signin.dart/sign_up.dart
@@ -1,3 +1,4 @@
+import 'package:doptica_app/core/utils/app_style.dart';
 import 'package:doptica_app/core/widgets/Cutomebutton.dart';
 import 'package:doptica_app/core/widgets/custome_container.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class SignUp extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(
-                height: 65,
+                height: 30,
               ),
               Center(
                 child: Image.asset(
@@ -26,7 +27,40 @@ class SignUp extends StatelessWidget {
                   height: 40,
                 ),
               ),
-              
+              const SizedBox(
+                height: 40,
+              ),
+              InkWell(
+                borderRadius: BorderRadius.circular(20),
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 291,
+                  height: 45,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 23,
+                      ),
+                      Image.asset(
+                        "assets/images/Google__G__logo.svg.png",
+                        height: 30,
+                        width: 30,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text(
+                        "Sign In With Google ",
+                        style: AppStyles.styleOpenSansRegular18,
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
