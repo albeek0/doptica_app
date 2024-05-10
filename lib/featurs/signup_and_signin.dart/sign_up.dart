c975cfeb6dc757b8a080caa8b65769b5690c65b4
abdalazez <abdalazezk@gmail.com>
2024-05-10 09:23:57 +0300

    add more images

--- lib/featurs/signup_and_signin.dart/sign_up.dart
+++ lib/featurs/signup_and_signin.dart/sign_up.dart
@@ -1,3 +1,5 @@
+import 'package:doptica_app/featurs/edit_profile_feature.dart/widget/custome_container.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SignUp extends StatelessWidget {
@@ -5,6 +7,21 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomeContainer(
+      widget: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/Logo Doptica f3 1.png",
+                // height: 500,
+                // width: 700,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
