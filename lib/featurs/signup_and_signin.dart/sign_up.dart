d8cb8af838478e13e59febcb685f47a22e526de3
abdalazez <abdalazezk@gmail.com>
2024-05-10 16:35:18 +0300

    crreate sign in with google file

--- lib/featurs/signup_and_signin.dart/sign_up.dart
+++ lib/featurs/signup_and_signin.dart/sign_up.dart
@@ -1,6 +1,7 @@
 import 'package:doptica_app/core/utils/app_style.dart';
 import 'package:doptica_app/core/widgets/Cutomebutton.dart';
 import 'package:doptica_app/core/widgets/custome_container.dart';
+import 'package:doptica_app/featurs/signup_and_signin.dart/widgets/sign_in_with_google.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatelessWidget {
@@ -30,37 +31,7 @@ class SignUp extends StatelessWidget {
               const SizedBox(
                 height: 40,
               ),
-              InkWell(
-                borderRadius: BorderRadius.circular(20),
-                onTap: () {},
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  width: 291,
-                  height: 45,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 2),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 23,
-                      ),
-                      Image.asset(
-                        "assets/images/Google__G__logo.svg.png",
-                        height: 30,
-                        width: 30,
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      const Text(
-                        "Sign In With Google ",
-                        style: AppStyles.styleOpenSansRegular18,
-                      )
-                    ],
-                  ),
-                ),
-              )
+              const SignInWithGoogle()
             ],
           ),
         ),
@@ -68,3 +39,5 @@ class SignUp extends StatelessWidget {
     );
   }
 }
+
+
