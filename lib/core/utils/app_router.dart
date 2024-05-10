1ef815c6c1056e16b980bfc499ebc3e05161fee0
abdalazez <abdalazezk@gmail.com>
2024-05-10 09:04:57 +0300

    add sign up to router file

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,14 +1,16 @@
 import 'package:doptica_app/featurs/edit_profile_feature.dart/edit_profile_view.dart';
 import 'package:doptica_app/featurs/home_feature/home_view.dart';
 import 'package:doptica_app/featurs/profile_feature/profile_view.dart';
+import 'package:doptica_app/featurs/signup_and_signin.dart/sign_up.dart';
 import 'package:doptica_app/featurs/tasks_featurs/tasks_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/TasksView';
-  static const kEditProfileView = '/';
+  static const kEditProfileView = '/EditProfileView';
   static const kProfileView = '/ProfileView';
-  static const kTasksView = '/kHomeView';
+  static const kTasksView = '/HomeView';
+   static const kSignUp = '/';
 
   static final router = GoRouter(
     routes: [
@@ -27,6 +29,9 @@ abstract class AppRouter {
       GoRoute(
         path: kTasksView,
         builder: (context, state) => const TasksView(),
+      ), GoRoute(
+        path: kSignUp,
+        builder: (context, state) => const SignUp(),
       ),
     ],
   );
