a42bb353a73427c22f0d7247664344f630bc252a
abdalazez <abdalazezk@gmail.com>
2024-05-22 17:19:59 +0300

    edit height on the app bar

--- lib/core/widgets/custom_app_bar_new.dart
+++ lib/core/widgets/custom_app_bar_new.dart
@@ -1,17 +1,22 @@
 import 'package:doptica_app/core/utils/app_style.dart';
 import 'package:flutter/material.dart';
+
 class CuotomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
-  Size get preferredSize => const Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(50);
   const CuotomeAppBar({
-    super.key, required this.label,
+    super.key,
+    required this.label,
   });
   final String label;
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.transparent,
-      title:  Text(label,style: AppStyles.styleOpenSansBold20,),
+      title: Text(
+        label,
+        style: AppStyles.styleOpenSansBold20,
+      ),
       actions: [
         IconButton(
           icon: const Icon(Icons.density_medium),
