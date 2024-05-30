9f55999cfd179ab462bb0c0085215a13d8739d84
abdalazez <abdalazezk@gmail.com>
2024-05-30 19:05:11 +0300

    non

--- lib/featurs/posts_feature/posts_screen.dart
+++ lib/featurs/posts_feature/posts_screen.dart
@@ -3,7 +3,6 @@ import 'package:doptica_app/core/widgets/custome_container.dart';
 import 'package:doptica_app/core/widgets/customenavbar.dart';
 import 'package:doptica_app/featurs/posts_feature/posts.dart';
 import 'package:flutter/material.dart';
-
 import '../../core/widgets/custom_app_bar_new.dart';
 
 class PostsScreen extends StatelessWidget {
