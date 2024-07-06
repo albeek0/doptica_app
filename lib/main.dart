9f2845d858ab9e3dc3f05ad6ff52eb73b4860960
abdalazez <abdalazezk@gmail.com>
2024-07-07 02:01:42 +0300

    finishing fire base

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:doptica_app/core/utils/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  Firebase.initializeApp;
   runApp(const DopticaApp());
 }
 
